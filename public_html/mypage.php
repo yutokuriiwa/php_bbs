2ba132ddfc13d8cd3cdccc57112b3d5403b56041
yuta.hanai <(hanaiyu22@gmail.com)>
2019-02-21 13:45:03 +0900

    ユーザー情報更新画面で値が正しく取れていない不具合を修正

--- public_html/mypage.php
+++ public_html/mypage.php
@@ -2,8 +2,6 @@
 require_once(__DIR__ .'/header.php');
 require_once(__DIR__ . '/redirect.php');
 
-// $user = new Bbs\Model\User();
-// $user->find();
 $app = new Bbs\Controller\UserUpdate();
 $app->run();
 ?>
@@ -20,11 +18,11 @@ $app->run();
       <input type="text" name="username" value="<?= isset($app->getValues()->username) ? h($app->getValues()->username): ''; ?>" class="form-control">
       <p class="err"><?= h($app->getErrors('username')); ?></p>
     </div>
-    <div class="form-group">
+    <!-- <div class="form-group">
       <label>アイコン画像</label>
       <input type="file" name="file" value="" class="form">
       <p class="err"></p>
-    </div>
+    </div> -->
     <p class="err"></p>
     <button class="btn btn-primary" onclick="document.getElementById('userupdate').submit();">更新</button>
     <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
